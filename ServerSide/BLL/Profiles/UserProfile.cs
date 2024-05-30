22e6dc3c8813386cd6e86b137ad5ddcaadd0c177
וינר רחל1 < 1@C9>
2024-05-30 14:37:25 +0300

    Profile #1

---
+++ ServerSide/BLL/Profiles/UserProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using BL.DTO;
+using DAL.Models;
+
+namespace BLL.Profiles
+{
+    public class UserProfile : Profile
+    {
+        public UserProfile()
+        {
+            CreateMap<User, UserDTO>().ReverseMap();
+        }
+    }
+}
