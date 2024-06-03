f75eeb67e16ec3a1f93072d973803df39338f621
אייזנברג חיה < @mbjcomp.local>
2024-06-03 19:48:00 +0300

    a few changes

--- ServerSide/ServerSide/Program.cs
+++ ServerSide/ServerSide/Program.cs
@@ -3,20 +3,52 @@ using DAL.Models;
 using DBAccess;
 using Microsoft.EntityFrameworkCore;
 
+//var builder = WebApplication.CreateBuilder(args);
+
+//builder.Services.AddControllers();
+
+
+
+
+
+//DBActions actions = new DBActions(builder.Configuration);
+//var connString = actions.GetConnectionString("CarsDB");
+//builder.Services.AddScoped<BLManeger>(b => new BLManeger(connString));
+////builder.Services.AddDbContext<CarsContext>(options => options.UseSqlServer(connString))/*;*/
+//var app = builder.Build();
+
+//app.MapGet("/", () => "Hello World!");
+//app.MapControllers();
+//app.Run();
+
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 
 
-
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("CORSPolicy",
+        builder =>
+        {
+            builder
+            .AllowAnyMethod()
+            .AllowAnyHeader()
+            .WithOrigins("http://localhost:3000");
+        });
+});
 
 
 DBActions actions = new DBActions(builder.Configuration);
 var connString = actions.GetConnectionString("CarsDB");
-builder.Services.AddScoped<BLManeger>(b => new BLManeger(connString));
-//builder.Services.AddDbContext<CarsContext>(options => options.UseSqlServer(connString))/*;*/
+builder.Services.AddScoped(b => new BLManeger(connString));
+
+
 var app = builder.Build();
 
+app.UseCors("CORSPolicy");
 app.MapGet("/", () => "Hello World!");
 app.MapControllers();
+
 app.Run();
